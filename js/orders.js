d598e3d416d77b9c5837c4a58f5cb1fc71e06ae3
hugomllo <hugomattusdd@gmail.com>
2022-07-25 23:36:17 -0300

    ultima atualização do projeto

---
+++ js/orders.js
@@ -0,0 +1,36 @@
+const Orders = [
+    {
+        productName: 'Mini Drone',
+        productNumber: '85441',
+        paymentStatus: 'Vencido',
+        shipping: 'Pendente'
+    },
+
+    {
+        productName: 'Drone Maior',
+        productNumber: '85442',
+        paymentStatus: 'Devolução',
+        shipping: 'Recusado'
+    },
+
+    {
+        productName: 'Drone Médio',
+        productNumber: '85443',
+        paymentStatus: 'Vencido',
+        shipping: 'Pendente'
+    },
+
+    {
+        productName: 'Combo Drone',
+        productNumber: '85444',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+    },
+
+    {
+        productName: 'Combo Drone Médio',
+        productNumber: '85445',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+    },
+]
--- js/script.js
+++ js/script.js
@@ -21,3 +21,18 @@ themeToggler.addEventListener('click', () =>{
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 })
+
+
+Orders.forEach(order => {
+    const tr =document.createElement('tr');
+    const trContent = `
+            <td>${order.productName}</td>
+            <td>${order.productNumber}</td>
+            <td>${order.paymentStatus}</td>
+            <td class="${order.shipping === 'Declined' ? 'danger' 
+                        : order.shipping === 'pending' ? 'warning' : 'primary'}">${order.shipping}</td>
+            <td class="primary">${order.productName}</td>
+    `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr)
+})
